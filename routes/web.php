fa3e2a2f9435c2e71e1fc1a8239732756a714a7e
yves <141734801+yvestandjiro@users.noreply.github.com>
2025-12-07 11:16:58 +0100

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,7 +8,9 @@ Route::get('/zuu', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('home');
+
+Route::get('/dashboards', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
